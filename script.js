379e3620346e74bd7dd41afbab97e85467dd1541
tony nolan <tonynolan313@gmail.com>
2020-09-30 19:44:14 -0600

    js code added

---
+++ script.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+
+$(".saveBtn").on("click", function(){
+    var value = $(this).siblings(".description").val()
+    var time = $(this).parent().attr("id")
+    localStorage.setItem(time,value)
+
+})
+
+function updateHour(){
+    var currentTime = moment().hours()
+
+    $(".time-block").each(function(){
+    var hour = parseInt($(this).attr("id").split("-")[1])
+    if(hour<currentTime){
+        $(this).addClass("past")
+
+    }else if (hour === currentTime){
+        $(this).removeClass("past")
+        $(this).addClass("present")
+    }else{
+        $(this).removeClass("past")
+        $(this).removeClass("present")
+        $(this).addClass("future")
+    }
+   });
+}
+updateHour()
+var interval = setInterval(updateHour, 15000)
+
+
+
+
+
+
+
+
+
+
+})
