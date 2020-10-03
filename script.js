da8dae827cb8b75a62907f9e6e6bcc99c1e61ed9
tony nolan <tonynolan313@gmail.com>
2020-10-03 11:58:09 -0600

    comments added

--- script.js
+++ script.js
@@ -1,43 +1,43 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-$(".saveBtn").on("click", function(){
-    var value = $(this).siblings(".description").val()
-    var time = $(this).parent().attr("id")
-    localStorage.setItem(time,value)
+    $(".saveBtn").on("click", function () {
+        var value = $(this).siblings(".description").val()
+        var time = $(this).parent().attr("id")
+        localStorage.setItem(time, value)
 
-})
+    })
+    // function to update the hour
+    function updateHour() {
+        var currentTime = moment().hours()
 
-function updateHour(){
-    var currentTime = moment().hours()
+        $(".time-block").each(function () {
+            var hour = parseInt($(this).attr("id").split("-")[1])
+            if (hour < currentTime) {
+                $(this).addClass("past")
 
-    $(".time-block").each(function(){
-    var hour = parseInt($(this).attr("id").split("-")[1])
-    if(hour<currentTime){
-        $(this).addClass("past")
-
-    }else if (hour === currentTime){
-        $(this).removeClass("past")
-        $(this).addClass("present")
-    }else{
-        $(this).removeClass("past")
-        $(this).removeClass("present")
-        $(this).addClass("future")
+            } else if (hour === currentTime) {
+                $(this).removeClass("past")
+                $(this).addClass("present")
+            } else {
+                $(this).removeClass("past")
+                $(this).removeClass("present")
+                $(this).addClass("future")
+            }
+        });
     }
-   });
-}
-updateHour()
-var interval = setInterval(updateHour, 15000)
-
-$("#hour-9 .description").val(localStorage.getItem("hour-9"))
-$("#hour-10 .description").val(localStorage.getItem("hour-10"))
-$("#hour-11 .description").val(localStorage.getItem("hour-11"))
-$("#hour-12 .description").val(localStorage.getItem("hour-12"))
-$("#hour-13 .description").val(localStorage.getItem("hour-13"))
-$("#hour-14 .description").val(localStorage.getItem("hour-14"))
-$("#hour-15 .description").val(localStorage.getItem("hour-15"))
-$("#hour-16 .description").val(localStorage.getItem("hour-16"))
-$("#hour-17 .description").val(localStorage.getItem("hour-17"))
-
-$("#currentDay").text(moment().format("dddd MMMM Do, YYYY"))
+    updateHour()
+    var interval = setInterval(updateHour, 15000)
+    // get items from local storage
+    $("#hour-9 .description").val(localStorage.getItem("hour-9"))
+    $("#hour-10 .description").val(localStorage.getItem("hour-10"))
+    $("#hour-11 .description").val(localStorage.getItem("hour-11"))
+    $("#hour-12 .description").val(localStorage.getItem("hour-12"))
+    $("#hour-13 .description").val(localStorage.getItem("hour-13"))
+    $("#hour-14 .description").val(localStorage.getItem("hour-14"))
+    $("#hour-15 .description").val(localStorage.getItem("hour-15"))
+    $("#hour-16 .description").val(localStorage.getItem("hour-16"))
+    $("#hour-17 .description").val(localStorage.getItem("hour-17"))
+    //set date and time to the top of the page
+    $("#currentDay").text(moment().format("dddd MMMM Do, YYYY"))
 
 })
